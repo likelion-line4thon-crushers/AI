a872e1db823b07a03b400b3ec4f7ecd07822f5bc
chaeelin <chaeee0123@gmail.com>
2025-11-08 18:20:40 +0900

    :memo: Chore: Swagger 설명 추가

--- routers/top_question_report.py
+++ routers/top_question_report.py
@@ -6,7 +6,10 @@ from models.common import BaseResponse, success
 
 router = APIRouter(prefix="/report", tags=["Report"])
 
-@router.get("/questions/rooms/{room_id}/top3", response_model=BaseResponse[TopQuestionReportResponse])
+@router.get("/questions/rooms/{room_id}/top3", response_model=BaseResponse[TopQuestionReportResponse],
+            summary="TOP3",
+            description="지정된 room_id의 질문들을 불러와 의미 유사도를 기반으로 묶은 **TOP3 질문 클러스터**를 반환합니다."
+)
 async def top3_report(room_id: str):
     questions = await list_room_questions(room_id)
     report = build_top3(questions)
